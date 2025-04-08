bd99bb811985ae5ff226b1a642edb9a7a6826d79
Sujal07 <chhajedsujal70@gmail.com>
2025-04-08 11:05:37 +0530

    first commit

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,67 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "React",
+    "Javascript",
+    "TailwindCSS",
+  ];
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Passionate  Frontend developer with expertise in building Responsive web
+              pages and designing creative UI components.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend </h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8 ml-60 w-full">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> B.Tech in Computer Engineering </strong> - K.K.Wagh University
+                  (2023-2026)
+                </li>
+                <li>
+                  Relevant Coursework: Data Structures, Web Development, Data Analytics.
+                </li>
+              </ul>
+            </div>
+           
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
